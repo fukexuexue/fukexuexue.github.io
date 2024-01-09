7ad5b3b433313c57e682a55038c7eefa4166386d
fukexuexue <fukexue1319365742@gmail.com>
2024-01-10 00:48:58 +0800

    12

--- assets/js/back.js
+++
@@ -1,6 +0,0 @@
-<script>
-  document.getElementById('back-to-previous').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.history.back(); // 执行返回上一个页面的操作
-  });
-</script>
