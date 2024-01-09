09269cbc21343911c0abccaa6bbf5eff8c5eefc8
fukexuexue <99808457+fukexuexue@users.noreply.github.com>
2024-01-09 23:40:14 +0800

    Add files via upload

---
+++ assets/js/back.js
@@ -0,0 +1,6 @@
+<script>
+  document.getElementById('back-to-previous').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.history.back(); // 执行返回上一个页面的操作
+  });
+</script>
